657cc5fc365a73df4a91982bebe2c555ddc2b1cc
NgurahFajar <96526566+NgurahFajar@users.noreply.github.com>
2022-01-03 18:15:52 +0800

    Add files via upload

---
+++ TipeDataNumber.java
@@ -0,0 +1,15 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniINT = 1000000;
+        long iniLong = 100000000L;
+        float iniFloat = 10.10f;
+        double iniDouble = 10.10;
+        int decimalInt = 25;
+        int hexInt = 0xFFFFFF;
+        int binInt = 0b01010101;
+        long balance = 1_000_000_000_000L;
+
+    }
+}
